5db3d4efe71f62dafcd2e1339dcffa4194eb3f4c
PiliDavila17 <93629747+PiliDavila17@users.noreply.github.com>
2022-05-30 17:40:24 -0500

    Add files via upload

--- Tienda de ropa.cpp
+++ Tienda de ropa.cpp
@@ -24,7 +24,7 @@ con el objetivo de agilizar su atención, para que posterior a realizar el filtro
 using namespace std;
 
 int main() {
-//Variables utilizadas
+	//Variables utilizadas
 	string ti, ta, fin, ep, ma, sec;
 	int o, op, total = 0, pant = 400, blu = 250;
 	int fal = 350, cham = 750;
@@ -32,7 +32,7 @@ int main() {
 	Carrito usuario;
 	bool running = true;
 
-//Comienza el menu de opciones donde el usuario escogerá las prendas que desea comprar, para posteriormente conocer el cobro total de su compra
+	//Comienza el menu de opciones donde el usuario escogerá las prendas que desea comprar, para posteriormente conocer el cobro total de su compra
 	while (running) {
 		//Impresion del menu
 		cout << "MENU \n\nQue prenda quieres? \n1. Pantalon \n2. Falda \n3. Blusa \n4. Chamarra \n5. Finalizar compra \n6. Imprimir Carrito \n7. Salir \n--> ";
@@ -47,7 +47,7 @@ int main() {
 
 			cout << "\nDescriba su pantalon ideal \n(Escriba su opcion con la palabra exacta)" << endl;
 
-			Pantalon pantalon_1("\nTalla: \nCH \nM \nG \nXL","\nTipo: \nMezclilla \nVestir \nCuero \nManta","\nEpoca: \n70s \n80s \n90s: ");
+			Pantalon pantalon_1("\nTalla: \nCH \nM \nG \nXL", "\nTipo: \nMezclilla \nVestir \nCuero \nManta", "\nEpoca: \n70s \n80s \n90s: ");
 			//Modifica el atributo talla	
 			cout << pantalon_1.getTalla() << " " << endl;
 			cout << "Introduzca la talla: ";
@@ -91,7 +91,7 @@ int main() {
 				cin >> ti;
 				falda_1.setTipo(ti);
 				//Modifica el atributo Seccion
-				cout << falda_1.getSeccion()<<endl;
+				cout << falda_1.getSeccion() << endl;
 				cout << "Introduzca la seccion: \n-Nino (N) \nAdulto(A)";
 				cin >> sec;
 
@@ -101,152 +101,156 @@ int main() {
 			}
 		//Funcion 3: Accesa a la clase Blusa en donde el usuario podra escoger el tipo de blusa que desea comprar, añadiendo las caracteristicas de
 		//talla, tipo y seccion. Se utilizaron getters y setters como metodos de acceso
-			else
-				if (o == 3)
-				{
-					total = total + blu; //Añade una blusa a la lista y va calculando el costo acumulado
-					contBlus++;
-					cout << "\nDescribe tu blusa ideal \n(Escriba su opcion con la palabra exacta)" << endl;
-					Blusa blusa_1("\nTalla: \nCH \nM \nG \nXL", "\nTipo: \nSe divide por corte de manga:\n Corta \nLarga \n3/4");
-					//Modifica el atributo de Talla
-					cout << blusa_1.getTalla() << " ";
-					cout << "\nIntroduzca la talla: ";
-					cin >> ta;
-					blusa_1.setTalla(ta);
-					//Modifica el atributo Tipo
-					cout << blusa_1.getTipo() << endl;
-					cout << "\nIntroduzca el tipo: ";
-					cin >> ti;
-					blusa_1.setTipo(ti);
-					//Modifica el atributo Seccion
-					cout << blusa_1.getSeccion()<<endl;
-					cout << "Introduzca la seccion: \n-Nino (N) \nAdulto(A)";
-					cin >> sec;
-					blusa_1.setSeccion(sec);
+		else
+			if (o == 3)
+			{
+				total = total + blu; //Añade una blusa a la lista y va calculando el costo acumulado
+				contBlus++;
+				cout << "\nDescribe tu blusa ideal \n(Escriba su opcion con la palabra exacta)" << endl;
+				Blusa blusa_1("\nTalla: \nCH \nM \nG \nXL", "\nTipo: \nSe divide por corte de manga:\n Corta \nLarga \n3/4");
+				//Modifica el atributo de Talla
+				cout << blusa_1.getTalla() << " ";
+				cout << "\nIntroduzca la talla: ";
+				cin >> ta;
+				blusa_1.setTalla(ta);
+				//Modifica el atributo Tipo
+				cout << blusa_1.getTipo() << endl;
+				cout << "\nIntroduzca el tipo: ";
+				cin >> ti;
+				blusa_1.setTipo(ti);
+				//Modifica el atributo Seccion
+				cout << blusa_1.getSeccion() << endl;
+				cout << "Introduzca la seccion: \n-Nino (N) \nAdulto(A)";
+				cin >> sec;
+				blusa_1.setSeccion(sec);
 
-					cout << "Se ha guardado su eleccion" << endl << endl;
-					usuario.agregaBlusa(blusa_1);
-				}
+				cout << "Se ha guardado su eleccion" << endl << endl;
+				usuario.agregaBlusa(blusa_1);
+			}
 		//Funcion 4: Accesa a la clase Chamarra en donde el usuario podra escoger el tipo de chamarra que desea comprar, añadiendo las caracteristicas de
 		//talla, tipo, seccion y material. Se utilizaron getters y setters como metodos de acceso
-				else
-					if (o == 4) {
-						total = total + cham;//Agrega una chamarra a la lista y va calculando el costo acumulado
-						contCham++;
-						cout << "\nDescribe tu Chamarra ideal: \n(Escriba su opcion con la palabra exacta" << endl;
-						cout << "¿Qué bloque de chamarras quieres?" << endl;
+		else
+			if (o == 4) 
+			{
+				total = total + cham;//Agrega una chamarra a la lista y va calculando el costo acumulado
+				contCham++;
+				cout << "\nDescribe tu Chamarra ideal: \n(Escriba su opcion con la palabra exacta" << endl;
+				cout << "¿Qué bloque de chamarras quieres?" << endl;
 
-						//Hay sobrecarga es por eso que se divide en dos, para cada tipo de constructor
-						cout << "\nBLOQUE 1: Talla y Tipo \nBLOQUE 2: Talla, Tipo, Material y Seccion \nSelecione 1 o 2" << endl;
-						cin >> op;
-						//primer constructor
-						if (op == 1) {
-							Chamarra chamarra_1("\nTalla: \nCH \nM \nG \nXL", "\nTipo: \nInvierno \nVerano");
-							//Modifica el atributo Talla
-							cout << chamarra_1.getTalla() << " ";
-							cout << "\nIntroduzca la talla: ";
-							cout << "\nTalla: \nCH \nM \nG \nXL" << endl;
-							cin >> ta;
-							chamarra_1.setTalla(ta);
-							//Modifica el atributo Tipo
-							cout << chamarra_1.getTipo() << endl;
-							cout << "\nIntroduzca el tipo: ";
-							cout << "\nTipo: \n Invierno \nVerano" << endl;
-							cin >> ti;
-							chamarra_1.setTipo(ti);
-						}
-						else 
-							if (op == 2) { //segundo constructor
-								Chamarra chamarra_1("\nTalla: \nCH \nM \nG \nXL", "\nTipo: \nInvierno \nVerano", "\nMaterial: \nCuero \nMezclilla", "\nSeccion: \nNiños \nAdultos");
-								//Modifica el atributo Talla
-								cout << chamarra_1.getTalla() << " ";
-								cout << "\nIntroduzca la talla: ";
-								cin >> ta;
-								chamarra_1.setTalla(ta);
-								//Modifica el atributo Tipo
-								cout << chamarra_1.getTipo() << endl;
-								cout << "\nIntroduzca el tipo: ";
-								cin >> ti;
-								chamarra_1.setTipo(ti);
-								//Modifica el atributo Material
-								cout << chamarra_1.getMaterial() << endl;
-								cout << "\nIntroduzca el Material: ";
-								cin >> ma;
-								chamarra_1.setMaterial(ma);
-								//Modifia el atributo Seccion
-								cout << chamarra_1.getSeccion() << endl;
-								cout << "Introduzca la seccion Nino (N) o Adulto(A): " << endl;
-								cin >> sec;
-								chamarra_1.setSeccion(sec);
-							}
-					}
-			//Funcion 5: Imprime el total de prendas acumuladas seleccionadas por el usuario, así como el costo final de su compra
-				else
-					if (o == 5)
+				//Hay sobrecarga es por eso que se divide en dos, para cada tipo de constructor
+				cout << "\nBLOQUE 1: Talla y Tipo \nBLOQUE 2: Talla, Tipo, Material y Seccion \nSelecione 1 o 2" << endl;
+				cin >> op;
+				//primer constructor
+				if (op == 1) 
+				{
+					Chamarra chamarra_1("\nTalla: \nCH \nM \nG \nXL", "\nTipo: \nInvierno \nVerano");
+					//Modifica el atributo Talla
+					cout << chamarra_1.getTalla() << " ";
+					cout << "\nIntroduzca la talla: ";
+					cout << "\nTalla: \nCH \nM \nG \nXL" << endl;
+					cin >> ta;
+					chamarra_1.setTalla(ta);
+					//Modifica el atributo Tipo
+					cout << chamarra_1.getTipo() << endl;
+					cout << "\nIntroduzca el tipo: ";
+					cout << "\nTipo: \n Invierno \nVerano" << endl;
+					cin >> ti;
+					chamarra_1.setTipo(ti);
+				}
+				else if (op == 2) 
+				{ //segundo constructor
+						Chamarra chamarra_1("\nTalla: \nCH \nM \nG \nXL", "\nTipo: \nInvierno \nVerano", "\nMaterial: \nCuero \nMezclilla", "\nSeccion: \nNiños \nAdultos");
+						//Modifica el atributo Talla
+						cout << chamarra_1.getTalla() << " ";
+						cout << "\nIntroduzca la talla: ";
+						cin >> ta;
+						chamarra_1.setTalla(ta);
+						//Modifica el atributo Tipo
+						cout << chamarra_1.getTipo() << endl;
+						cout << "\nIntroduzca el tipo: ";
+						cin >> ti;
+						chamarra_1.setTipo(ti);
+						//Modifica el atributo Material
+						cout << chamarra_1.getMaterial() << endl;
+						cout << "\nIntroduzca el Material: ";
+						cin >> ma;
+						chamarra_1.setMaterial(ma);
+						//Modifia el atributo Seccion
+						cout << chamarra_1.getSeccion() << endl;
+						cout << "Introduzca la seccion Nino (N) o Adulto(A): " << endl;
+						cin >> sec;
+						chamarra_1.setSeccion(sec);
+				}
+			}
+		//Funcion 5: Imprime el total de prendas acumuladas seleccionadas por el usuario, así como el costo final de su compra
+		else if (o == 5)
+				{
+					cout << "Ha finalizado su compra! \n\n GRACIAS POR SU PREFERENCIA \n\nQuiere proceder al pago? (s/n): ";
+					cin >> fin;
+
+					if (fin == "s")
 					{
-						cout << "Ha finalizado su compra! \n\n GRACIAS POR SU PREFERENCIA \n\nQuiere proceder al pago? (s/n): ";
-						cin >> fin;
-
-						if (fin == "s")
-						{
-							cout << "\nGRACIAS POR SU COMPRA!" << endl;
-							cout << "El total es: $" << total << endl;
-							cout << "Imprima su carrito para proseguir con la compra" << endl << endl;
-						}
-						else
-							if (fin == "n")
-							{
-								cout << "Continuemos" << endl;
-							}
-							else
-								cout << "OpciÃ³n invalida, intentelo de nuevo" << endl << endl;
+						cout << "\nGRACIAS POR SU COMPRA!" << endl;
+						cout << "El total es: $" << total << endl;
+						cout << "Imprima su carrito para proseguir con la compra" << endl << endl;
+					}
+					else if (fin == "n")
+					{
+						cout << "Continuemos" << endl;
+					}
+					else 
+					{
+						cout << "Opcion invalida, intentelo de nuevo" << endl << endl;
 					}
 
-			//Funcion 6: Accede a la clase Carrito para imprimir cuales son las prendas seleccionadas por el usuario
-					else
-						if (o == 6)
-						{
-							cout << "\nEL CARRITO CONTIENE: " << endl;
-							//Conteo de blusas
-							cout << "Total de blusas: " << contBlus << endl;
-							for (int i = 0; i < usuario.getpos_blus(); i++) {
-								cout << "Blusa " << i + 1 << endl;
-								cout << "\nTalla: " << usuario.getBlusa(i).getTalla();
-								cout << "\nTipo de manga: " << usuario.getBlusa(i).getTipo() << endl << endl;
-							}
-							//Conteo de faldaas
-							cout << "Total de faldas: " << contFal << endl;
-							for (int i = 0; i < usuario.getpos_fal(); i++) {
-								cout << "Falda " << i + 1 << endl;
-								cout << "Talla: " << usuario.getFalda(i).getTalla() << endl;
-								cout << "Tipo: " << usuario.getFalda(i).getTipo() << endl << endl;
-							}
-							//Conteo de pantalones
-							cout << "Total de pantalones: " << contPant << endl;
-							for (int i = 0; i < usuario.getpos_pant(); i++) {
-								cout << "Pantalon " << i + 1 << endl;
-								cout << "Talla: " << usuario.getPantalon(i).getTalla() << endl;
-								cout << "Tipo: " << usuario.getPantalon(i).getTipo() << endl;
-								cout << "Epoca: " << usuario.getPantalon(i).getEpoca() << endl << endl;
-							}
-							//Conteo de Chamarras
-							cout <<"Total de Chamarras: " << contCham <<endl;
-							for (int i=0; i < usuario.getpos_cham(); i++) {
-								cout << "Chamarra " << i + 1 << endl;
-								cout << "Material: " << usuario.getChamarra(i).getMaterial() << endl;
-								cout << "Sec: " << usuario.getChamarra(i).getSeccion() << endl;
-								}
-							}
-						}
-				//Funcion 7: Salida
-						else
-							if (o == 7)
-							{
-								cout << "\nPase a la caja a pagar y recoger sus prendas. \nGracias por venir \nVUELVA PRONTO" << endl << endl;
-								running = false;
-								break; //Cierra el ciclo
-							}
+				}
+
+		//Funcion 6: Accede a la clase Carrito para imprimir cuales son las prendas seleccionadas por el usuario
+		else if (o == 6)
+			{
+				cout << "\nEL CARRITO CONTIENE: " << endl;
+				//Conteo de blusas
+				cout << "Total de blusas: " << contBlus << endl;
+				for (int i = 0; i < usuario.getpos_blus(); i++) 
+				{
+					cout << "Blusa " << i + 1 << endl;
+					cout << "\nTalla: " << usuario.getBlusa(i).getTalla();
+					cout << "\nTipo de manga: " << usuario.getBlusa(i).getTipo() << endl << endl;
+				}
+				//Conteo de faldas
+				cout << "Total de faldas: " << contFal << endl;
+				for (int i = 0; i < usuario.getpos_fal(); i++) {
+					cout << "Falda " << i + 1 << endl;
+					cout << "Talla: " << usuario.getFalda(i).getTalla() << endl;
+					cout << "Tipo: " << usuario.getFalda(i).getTipo() << endl << endl;
+				}
+				//Conteo de pantalones
+				cout << "Total de pantalones: " << contPant << endl;
+				for (int i = 0; i < usuario.getpos_pant(); i++) {
+					cout << "Pantalon " << i + 1 << endl;
+					cout << "Talla: " << usuario.getPantalon(i).getTalla() << endl;
+					cout << "Tipo: " << usuario.getPantalon(i).getTipo() << endl;
+					cout << "Epoca: " << usuario.getPantalon(i).getEpoca() << endl << endl;
+				}
+				//Conteo de Chamarras
+				cout << "Total de Chamarras: " << contCham << endl;
+				for (int i = 0; i < usuario.getpos_cham(); i++) {
+					cout << "Chamarra " << i + 1 << endl;
+					cout << "Material: " << usuario.getChamarra(i).getMaterial() << endl;
+					cout << "Sec: " << usuario.getChamarra(i).getSeccion() << endl;
+				}
+			}
+
+			//Funcion 7: Salida
+		else
+			if (o==7)
+			{
+					cout << "\nPase a la caja a pagar y recoger sus prendas. \nGracias por venir \nVUELVA PRONTO" << endl << endl;
+					running = false;
+			}
+	
 	}
-	return 0;
+	
 }
+
 
